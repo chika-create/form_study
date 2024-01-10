94d9d1f2e9e0eabe8b88d2b1d3730ac9cdfad4f0
安井 智香 <yasui.chika@a-tm.co.jp>
2024-01-10 19:00:07 +0900

    add: ラジオボタンを追加など

--- src/index.tsx
+++ src/index.tsx
@@ -31,6 +31,7 @@ function App () {
   return (
     <div>
       <section>
+        <h1>クレジットカードを検索する</h1>
         <div>
           <h2>いつまでに欲しい？</h2>
           <select onChange={event => setSelectValue(event.target.value)}>
@@ -40,20 +41,38 @@ function App () {
           </select>
         </div>
         <div>
+          <h2>希望の国際ブランドは？</h2>
           JCB <input type="checkbox" value="jcb" onChange={handleCheckboxChange} /><br />
           VISA <input type="checkbox" value="visa" onChange={handleCheckboxChange} /><br />
           Master Card <input type="checkbox" value="mastercard" onChange={handleCheckboxChange} />
         </div>
         <div>
+          <h2>ご自身の年齢は？</h2>
+          <label>
+            <input type="radio" value="25歳以下" />
+            25歳以下
+          </label>
+          <label>
+            <input type="radio" value="25〜35歳" />
+            25〜35歳
+          </label>
+          <label>
+            <input type="radio" value="35歳〜" />
+            35歳〜
+          </label>
+        </div>
+        <div>
+          <h2>感想</h2>
           <input type="text" value={textBoxValue} onChange={handleTextBoxChange} />
         </div>
       </section>
       <section>
-        <h2>変更された値</h2>
+        <h1>変更された値</h1>
         <ul>
           <li>いつまでに欲しい？：{selectValue}</li>
           <li>選択されたチェックボックス：{checkValue.join(", ")}</li>
-          <li>入力された内容：{textBoxValue}</li>
+          <li>選択されたラジオボタン：</li>
+          <li>感想に入力された内容：{textBoxValue}</li>
         </ul>
       </section>
     </div>
