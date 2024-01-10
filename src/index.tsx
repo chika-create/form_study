8dc2b056433d0a8829b997b6adf2f0728e10d0bf
安井 智香 <yasui.chika@a-tm.co.jp>
2024-01-10 09:54:50 +0900

    update: チェックボックスが選択されたらstateを変更

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,21 @@ import './index.css';
 
 function App () {
   const [selectValue, setSelectValue] = useState<string | null>(null);
-  const [chackValue, setCheckValue] = useState<string | null>(null);
+  const [chackValue, setCheckValue] = useState<string[]>([]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      // チェックが付いた場合、配列に追加
+      setCheckValue(prevValues => [...prevValues, value]);
+    } else {
+      // チェックが外れた場合、配列から削除
+      // prevValues配列からvalueと一致する要素を除外し、新しい配列を作成
+      setCheckValue(prevValues => prevValues.filter(item => item !== value));
+    }
+  };
 
   return (
     <section>
@@ -18,9 +32,9 @@ function App () {
         </select>
       </div>
       <div>
-        JCB <input type="checkbox" value="jcb" /><br />
-        VISA <input type="checkbox" value="visa" /><br />
-        Master Card <input type="checkbox" value="mastercard" />
+        JCB <input type="checkbox" value="jcb" onChange={handleCheckboxChange} /><br />
+        VISA <input type="checkbox" value="visa" onChange={handleCheckboxChange} /><br />
+        Master Card <input type="checkbox" value="mastercard" onChange={handleCheckboxChange} />
       </div>
     </section>
   );
